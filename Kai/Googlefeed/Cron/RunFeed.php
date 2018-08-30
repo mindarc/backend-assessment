79a866fb5fe31d97440ac17795dac3dedad5f79e
Kai <gabazoo@gmail.com>
2018-08-30 23:13:49 +1000

    First Commit, Initial Googlefeed model, Write feed to file and Cron

---
+++ Kai/Googlefeed/Cron/RunFeed.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Kai\Googlefeed\Cron;
+
+/**
+ * class RunFeed
+ *
+ * Cron + Logger to generate google feed into pub/media
+ *
+ * @author Cheng Shea kai <gabazoo@gmail.com>
+ * @see https://github.com/bugcskai/
+ */
+class RunFeed {
+
+  private $logger;
+
+  private $feed;
+
+  public function __construct(
+    \Psr\Log\LoggerInterface $logger,
+    \Kai\Googlefeed\Model\Feed $feed) {
+
+    $this->logger = $logger;
+    $this->feed = $feed;
+
+  }
+
+
+  /**
+   * Output feed to file 
+   *
+   * @return string XML output
+   */
+  public function execute() {
+    $this->logger->info('Google Feed Cron Start');
+    try {
+      $_objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+
+      $filesystem = $_objectManager->get('Magento\Framework\Filesystem');
+      $directoryList = $_objectManager->get('Magento\Framework\App\Filesystem\DirectoryList');
+
+      $feed = $this->feed->generateFeed();
+
+      $media = $filesystem->getDirectoryWrite($directoryList::MEDIA);
+      $media->writeFile("kaigooglefeed/feed.xml",$feed);
+    }
+    catch(\Exception $e){
+      $this->logger->info($e->getMessage());
+    }
+
+    $this->logger->info('Google Feed Cron End');
+
+  }
+
+}
